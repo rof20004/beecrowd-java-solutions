9595ed2a7bbd1b0c6e52e2f0a06d8ea5b426d82a
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2022-06-26 23:28:12 -0400

    feat: add challenge 1000

---
+++ src/com/beecrowd/_1000/Main.java
@@ -0,0 +1,12 @@
+package com.beecrowd._1000;
+
+/**
+ * beecrowd challenge 1000
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
--- src/com/beecrowd/_1046/Main.java
+++ src/com/beecrowd/_1046/Main.java
@@ -3,7 +3,7 @@ package com.beecrowd._1046;
 import java.util.Scanner;
 
 /**
- * Challenge 1046
+ * beecrowd challenge 1046
  */
 public class Main {
 
@@ -13,17 +13,21 @@ public class Main {
         var startTime = scanner.nextInt();
         var endTime = scanner.nextInt();
 
-        if (is24HoursDuration(startTime, endTime)) {
-            System.out.println("O JOGO DUROU 24 HORA (S)");
-        } else if (isStartTimeLessThanEndTime(startTime, endTime)) {
-            System.out.format("O JOGO DUROU %d HORA (S)\n", endTime - startTime);
-        } else {
-            var totalTime = (24 - startTime) + endTime;
-            System.out.format("O JOGO DUROU %d HORA (S)\n", totalTime);
+        if (isEqualTime(startTime, endTime)) {
+            System.out.println("O JOGO DUROU 24 HORA(S)");
+            return;
         }
+
+        if (isStartTimeLessThanEndTime(startTime, endTime)) {
+            System.out.printf("O JOGO DUROU %d HORA(S)\n", endTime - startTime);
+            return;
+        }
+
+        var totalTime = (24 - startTime) + endTime;
+        System.out.printf("O JOGO DUROU %d HORA(S)\n", totalTime);
     }
 
-    private static boolean is24HoursDuration(final int start, final int end) {
+    private static boolean isEqualTime(final int start, final int end) {
         return start == end;
     }
 
